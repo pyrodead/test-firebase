72d31402bdb1a66389e839d2f736e1ea6376f3ff
andrii naumkin <bobmads@gmail.com>
2021-03-22 16:09:47 +0200

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCcuKm8VTIXc0uBh2fQQ_CF2MetmKj5MzA",
+    authDomain: "test-firebase-122a2.firebaseapp.com",
+    projectId: "test-firebase-122a2",
+    storageBucket: "test-firebase-122a2.appspot.com",
+    messagingSenderId: "139254806867",
+    appId: "1:139254806867:web:e1f64df33b15011fa909a6"
+};
